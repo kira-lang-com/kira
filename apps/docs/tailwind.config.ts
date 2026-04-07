2a90f32a613b02a73f050f01f048d65c59605b5a
Priam Chambouvet <aipriamc@gmail.com>
2026-04-07 16:38:54 +0200

    Remove apps folder (docs moved to kira-doc repo)

--- apps/docs/tailwind.config.ts
+++
@@ -1,8 +0,0 @@
-import type { Config } from "tailwindcss";
-
-export default {
-  content: [
-    "./app/**/*.{js,jsx,ts,tsx}",
-    "./node_modules/fumadocs-ui/dist/**/*.{js,jsx,ts,tsx}",
-  ],
-} satisfies Config;
